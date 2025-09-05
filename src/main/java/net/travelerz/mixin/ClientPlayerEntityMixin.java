3ff3bb8d5240c64f76d048e979203275acdddf8e
Globox1997 <Micha.Damaschke@web.de>
2025-09-05 10:42:45 +0200

    unique annotation

--- src/main/java/net/travelerz/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/travelerz/mixin/ClientPlayerEntityMixin.java
@@ -4,6 +4,7 @@ import com.mojang.authlib.GameProfile;
 
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,6 +26,7 @@ public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
         super(world, profile);
     }
 
+    @Unique
     @Nullable
     private Biome travelerBiome;
 
